dd0078ee4d5084536f471e1e6989f7ed6314acdc
capellinx <capellaaa7@gmail.com>
2025-09-08 20:09:05 -0300

    feat: add testing

--- src/presentation/pages/auth/login/components/login-form.tsx
+++ src/presentation/pages/auth/login/components/login-form.tsx
@@ -85,7 +85,7 @@ export function LoginForm({ form, isLoading }: LoginProps) {
                className="w-full bg-primary text-white cursor-pointer"
                disabled={isLoading}
             >
-               {isLoading ? "Entrando..." : "Entrar"}
+               {isLoading ? "Entrando..." : "Entraaaaar"}
             </Button>
 
             <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
